15288cbf4f97e8c75aad7dfb489520da4c30f949
idlebo <shili_c@163.com>
2022-08-10 02:48:25 +0000

    add pmon test

--- test_pmon.sh
+++ test_pmon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "cache without delay"
+echo "pmon without delay"
 
-make vsim -j VSIM_ARGS="-m vivado/test_new_instr/tlb.coe -f tlb.fst" || exit
+make vsim -j VSIM_ARGS="-m vivado/test_new_instr/gzrom.coe -f pmon.fst" || exit
 # echo "cache with delay"
 # make vsim -j VSIM_ARGS="-m vivado/test_new_instr/cache.coe -p 0.99  "  || exit
